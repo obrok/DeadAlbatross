50dac5a5e9e421717f8bd96947313c4bca493440
obrok <pawel.obrok@infiniteloop.eu>
2010-04-10 17:45:27 +0200

    Initial commit

---
+++ Commons/Share.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeadAlbatross.Commons
+{
+    public struct Share
+    {
+        public String Name { get; set; }
+        public int Size { get; set; }
+    }
+}
