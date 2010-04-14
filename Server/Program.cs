ad99a95db488e63be2cf15c0e28291bda5f05bc1
Pawe   Obrok <pawel.obrok@gmail.com>
2010-04-14 11:52:38 +0200

    Komunikujacy sie klient i serwer

---
+++ Server/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace DeadAlbatross.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri baseAddress = new Uri("http://localhost:1337/DeadAlbatross/Server");
+            ServiceHost selfHost = new ServiceHost(typeof(Server), baseAddress);
+
+            try
+            {
+                selfHost.AddServiceEndpoint(typeof(Server), new WSHttpBinding(), "DeadAlbatrossServer");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+
+                selfHost.Open();
+                Console.WriteLine("The server is running.");
+                Console.WriteLine("Press <ENTER> to terminate service.");
+                Console.WriteLine();
+                Console.ReadLine();
+
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
+        }
+    }
+}
---
+++ Server/Server.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using DeadAlbatross.Commons;
+
+namespace DeadAlbatross.Server
+{
+    [ServiceContract(Namespace = "http://DeadAlbatross.Server")]
+    class Server
+    {
+        private static List<Share> _shares = new List<Share>();
+
+        [OperationContract]
+        public Share[] ListShares()
+        {
+            return _shares.ToArray();
+        }
+
+        [OperationContract]
+        public void ReportShares(Share[] shares)
+        {
+            System.Console.WriteLine("got " + _shares.Count + " shares");
+            _shares.AddRange(shares);
+        }
+    }
+}
